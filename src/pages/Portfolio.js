f0c630bc2f43e925ce7b9174d14937b86985036c
vinitha <selvivini@gmail.com>
2021-04-28 12:37:47 -0500

    added portfolio component

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import {Container, Card, Row, Col, Button} from 'react-bootstrap'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import projects from '../data/projects.json'
+
 const Portfolio = () => {
     return (
         <>
      <Header/>  
+    <Container>
+        <Row>
+        {projects.map(project=>{
+            return (<Col md={6} lg={4}>
+                <Card className="m-3" border="danger" style={{background: "#FFFDE7"}}>
+                    <Card.Body>
+                        <Card.Img src={project.image} height="200px"/>
+                        <Card.Title className="text-danger">{project.name}</Card.Title>
+                        <hr></hr>
+                        <Card.Subtitle className= "text-primary">{project.description}</Card.Subtitle>
+                        <Card.Text className= "text-info">{project.technologies}</Card.Text>
+                        <Col>
+                        <a href={project.deployedLink} target="_blank"rel="noreferrer"><Button className="m-1" variant="success">Deployed Link</Button></a>
+                        
+                       <a href={project.repository} target="_blank"rel="noreferrer"><Button variant="dark">GithubRepo</Button></a> 
+                        </Col>
+                    </Card.Body>
+               </Card>
+            </Col>
+            )
+        }
+            
+        )}
+        </Row>
+     </Container>
      <Footer/> 
      </>
     )
